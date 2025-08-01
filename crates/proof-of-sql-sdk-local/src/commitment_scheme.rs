d35fc4dd28bfceeb7c280b581aecf6f6263b8fe9
Ian Joiner <14581281+iajoiner@users.noreply.github.com>
2025-08-01 13:57:53 -0400

    feat: add `CommitmentEvaluationProofId::deserialize_verifier_setup`

--- crates/proof-of-sql-sdk-local/src/commitment_scheme.rs
+++ crates/proof-of-sql-sdk-local/src/commitment_scheme.rs
@@ -1,10 +1,13 @@
 use crate::{
     prover, sxt_chain_runtime::api::runtime_types::proof_of_sql_commitment_map::commitment_scheme,
 };
+use ark_serialize::{CanonicalDeserialize, Compress, Validate};
+use nova_snark::provider::hyperkzg::VerifierKey;
 use proof_of_sql::{
     base::commitment::CommitmentEvaluationProof,
     proof_primitive::{
-        dory::DynamicDoryEvaluationProof, hyperkzg::HyperKZGCommitmentEvaluationProof,
+        dory::{DynamicDoryEvaluationProof, VerifierSetup},
+        hyperkzg::{HyperKZGCommitmentEvaluationProof, HyperKZGEngine},
     },
 };
 use serde::{Deserialize, Serialize};
@@ -44,11 +47,33 @@ pub trait CommitmentEvaluationProofId:
 {
     /// The [`CommitmentScheme`] associated with this commitment type.
     const COMMITMENT_SCHEME: CommitmentScheme;
+
+    /// Deserialize the verifier public setup from bytes.
+    fn deserialize_verifier_setup<'a>(
+        bytes: &[u8],
+    ) -> Result<<Self as CommitmentEvaluationProof>::VerifierPublicSetup<'a>, String>;
 }
 
 impl CommitmentEvaluationProofId for HyperKZGCommitmentEvaluationProof {
     const COMMITMENT_SCHEME: CommitmentScheme = CommitmentScheme::HyperKzg;
+
+    fn deserialize_verifier_setup<'a>(bytes: &[u8]) -> Result<VerifierKey<HyperKZGEngine>, String> {
+        bincode::serde::decode_from_slice(
+            bytes,
+            bincode::config::legacy()
+                .with_fixed_int_encoding()
+                .with_big_endian(),
+        )
+        .map(|(setup, _)| setup)
+        .map_err(|e| format!("Failed to deserialize verifier setup: {}", e))
+    }
 }
+
 impl CommitmentEvaluationProofId for DynamicDoryEvaluationProof {
     const COMMITMENT_SCHEME: CommitmentScheme = CommitmentScheme::DynamicDory;
+
+    fn deserialize_verifier_setup<'a>(bytes: &[u8]) -> Result<VerifierSetup, String> {
+        VerifierSetup::deserialize_with_mode(bytes, Compress::No, Validate::No)
+            .map_err(|e| format!("Failed to deserialize verifier setup: {}", e))
+    }
 }
