207be449a18dcd76696175a083745ff09fb060de
stuarttimwhite <stuart.white@spaceandtime.io>
2025-09-08 10:41:08 -0400

    feat: csv

--- src/query_and_verify.rs
+++ src/query_and_verify.rs
@@ -1,6 +1,13 @@
 use crate::{base::CommitmentScheme, native::SxTClient};
+use arrow_csv::WriterBuilder;
 use clap::Args;
-use datafusion::arrow::{record_batch::RecordBatch, util::pretty::pretty_format_batches};
+use datafusion::arrow::{
+    array::{BinaryArray, FixedSizeBinaryArray, LargeBinaryArray, StringArray},
+    datatypes::DataType,
+    record_batch::RecordBatch,
+    util::pretty::pretty_format_batches,
+};
+use std::{path::PathBuf, sync::Arc};
 use subxt::utils::H256;
 
 #[derive(Args, Debug, Clone, PartialEq, Eq)]
@@ -74,6 +81,10 @@ pub struct QueryAndVerifySdkArgs {
         help = "Path to the verifier setup file. If not provided, defaults to the appropriate verifier setup for the selected commitment scheme."
     )]
     pub verifier_setup: Option<String>,
+
+    /// The results will be put in a csv at the output path. If `None`, no csv will be saved
+    #[arg(long)]
+    pub csv_file_path: Option<PathBuf>,
 }
 
 impl From<&QueryAndVerifySdkArgs> for (SxTClient, CommitmentScheme) {
@@ -91,6 +102,49 @@ impl From<&QueryAndVerifySdkArgs> for (SxTClient, CommitmentScheme) {
     }
 }
 
+fn cast_record_batch_to_csv_friendly_record_batch(record_batch: RecordBatch) -> RecordBatch {
+    RecordBatch::try_from_iter(
+        record_batch
+            .schema()
+            .fields()
+            .iter()
+            .zip(record_batch.columns().iter())
+            .map(|(field, arr)| {
+                (
+                    field.name(),
+                    match field.data_type().clone() {
+                        DataType::LargeBinary => Arc::new(StringArray::from(
+                            arr.as_any()
+                                .downcast_ref::<LargeBinaryArray>()
+                                .expect("Array should be LargeBinary")
+                                .into_iter()
+                                .map(|bin| hex::encode(bin.unwrap()))
+                                .collect::<Vec<_>>(),
+                        )),
+                        DataType::FixedSizeBinary(_) => Arc::new(StringArray::from(
+                            arr.as_any()
+                                .downcast_ref::<FixedSizeBinaryArray>()
+                                .expect("Array should be FixedSizeBinary")
+                                .into_iter()
+                                .map(|bin| hex::encode(bin.unwrap()))
+                                .collect::<Vec<_>>(),
+                        )),
+                        DataType::Binary => Arc::new(StringArray::from(
+                            arr.as_any()
+                                .downcast_ref::<BinaryArray>()
+                                .expect("Array should be BinaryArray")
+                                .into_iter()
+                                .map(|bin| hex::encode(bin.unwrap()))
+                                .collect::<Vec<_>>(),
+                        )),
+                        _ => arr.clone(),
+                    },
+                )
+            }),
+    )
+    .unwrap()
+}
+
 pub async fn query_and_verify(
     args: QueryAndVerifySdkArgs,
 ) -> Result<(), Box<dyn core::error::Error>> {
@@ -102,7 +156,51 @@ pub async fn query_and_verify(
         .await?
         .try_into()?;
 
+    if let Some(path) = args.csv_file_path {
+        let cast_result = cast_record_batch_to_csv_friendly_record_batch(result.clone());
+        // Write to CSV
+        let mut file_write = std::fs::File::create(path)?;
+        let mut writer = WriterBuilder::new().build(&mut file_write);
+        writer.write(&cast_result)?;
+    }
+
     // Print the result of the query
     println!("Query result:\n{}", pretty_format_batches(&[result])?);
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::query_and_verify::cast_record_batch_to_csv_friendly_record_batch;
+    use datafusion::arrow::array::{
+        ArrayRef, BinaryArray, FixedSizeBinaryArray, LargeBinaryArray, RecordBatch, StringArray,
+    };
+    use std::sync::Arc;
+
+    #[test]
+    fn we_can_cast_binary_to_string() {
+        let bin_collection: &[u8] = &[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let string_array: ArrayRef = Arc::new(StringArray::from(vec![hex::encode(bin_collection)]));
+        let large_binary_array: ArrayRef =
+            Arc::new(LargeBinaryArray::from_vec(vec![bin_collection]));
+        let binary_array: ArrayRef = Arc::new(BinaryArray::from_vec(vec![bin_collection]));
+        let fixed_size_binary_array: ArrayRef =
+            Arc::new(FixedSizeBinaryArray::from(vec![bin_collection]));
+        let record_batch = RecordBatch::try_from_iter(vec![
+            ("large", large_binary_array),
+            ("small", binary_array),
+            ("fixed", fixed_size_binary_array),
+            ("string", string_array.clone()),
+        ])
+        .unwrap();
+        let cast_record_batch = cast_record_batch_to_csv_friendly_record_batch(record_batch);
+        let expected_record_batch = RecordBatch::try_from_iter(vec![
+            ("large", string_array.clone()),
+            ("small", string_array.clone()),
+            ("fixed", string_array.clone()),
+            ("string", string_array),
+        ])
+        .unwrap();
+        assert_eq!(cast_record_batch, expected_record_batch);
+    }
 }
