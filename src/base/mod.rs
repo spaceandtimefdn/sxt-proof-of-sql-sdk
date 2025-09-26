95e8e8c93ead72e2334106c9f3ec5cb86b5f3b5d
stuarttimwhite <stuart.white@spaceandtime.io>
2025-09-26 13:58:08 -0400

    feat: `run_zk_query`

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -1,6 +1,6 @@
 #![doc = include_str!("README.md")]
 
-mod serde;
+pub(crate) mod serde;
 /// subxt-generated code for interacting with the sxt-chain runtime
 pub mod sxt_chain_runtime;
 
@@ -24,7 +24,9 @@ pub use prover_query::{
 };
 
 mod verify;
-pub use verify::{verify_prover_response, VerifyProverResponseError};
+pub use verify::{
+    verify_prover_response, verify_prover_via_gateway_response, VerifyProverResponseError,
+};
 
 /// tonic-generated code for interacting with the prover service
 pub mod prover {
