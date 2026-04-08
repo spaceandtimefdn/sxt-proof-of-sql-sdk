15ec0e6b18c14c3f7fe8a74a27647cce61f7457a
stuarttimwhite <stuart.white@spaceandtime.io>
2026-04-08 10:25:38 -0400

    feat: complete `sxt-proof-of-sql-cre-sdk-typescript` package

---
+++ sxt-proof-of-sql-cre-sdk-typescript/src/index.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+import { proofOfSqlResultSchema } from './proof-of-sql-result-schema/proof-of-sql-result-schema'
+
+const proofOfSqlSchema = z.object({
+	verify: z
+		.function()
+		.args(z.string(), z.array(z.string()))
+		.returns(z.string().transform((val) => proofOfSqlResultSchema.parse(JSON.parse(val)))),
+})
+
+export type ProofOfSqlResult = z.infer<typeof proofOfSqlSchema>
+
+declare global {
+	// eslint-disable-next-line no-var
+	var proofOfSql: ProofOfSqlResult
+}
+
+const obj = (globalThis as Record<string, unknown>)['proofOfSql']
+export const proofOfSql = () => proofOfSqlSchema.parse(obj)
